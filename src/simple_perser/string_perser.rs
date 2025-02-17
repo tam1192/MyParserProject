7294233ac7c3d2fefdc5ed0ab89c7f6ef49df7dd
nikki9750 <76221172+tam1192@users.noreply.github.com>
2025-02-17 17:42:37 +0900

    パーサーのベースを考えた

--- src/simple_perser/string_perser.rs
+++ src/simple_perser/string_perser.rs
@@ -1,9 +1,8 @@
-fn ex() -> String {
-    let s = "hello";
-    s.to_string()
+fn ex(input: &str) -> (&str, &str) {
+    return ("", input);
 }
 
 #[test]
 fn test_ex() {
-    assert_eq!(ex(), "hello");
+    assert_eq!(ex("abc"), ("", "abc"));
 }
