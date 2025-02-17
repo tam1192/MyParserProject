4168ede24513c654d21df4278ebda94288bf47c2
nikki9750 <76221172+tam1192@users.noreply.github.com>
2025-02-17 18:57:39 +0900

    split_string関数をchar関数にリファクタリングし、関連するテストを更新

--- src/simple_perser/string_perser.rs
+++ src/simple_perser/string_perser.rs
@@ -1,59 +1,34 @@
-type Output<'a> = (Option<&'a str>, &'a str);
+// type Parser<'a> = Box<dyn Fn() -> Output<'a>>;
+
 
 /// 文字列を指定したパターンで分割する
-fn split_string<'a>(input: &'a str, pattern: char) -> Output<'a> 
-{
-    assert!(input.len() > 0);
-    match input.find(pattern) {
-        Some(len) => (Some(&input[..len]), &input[len+1..]),
-        None => (None, input),
+fn char(input: &str, char: char) -> Option<((), &str)> {
+    if let Some(c) = input.chars().next() {
+        if c == char {
+            return Some(((), &input[1..]));
+        }
     }
+    return None;
 }
 
 #[cfg(test)]
 mod tests_ex {
-    use super::split_string;
+    use super::char;
     #[test]
-    fn test1() {
-        // ' 'で区切る場合
-        let input = "hello world !!!";
-        let output =  (Some("hello"), "world !!!");
-        let seperated = ' ';
-        assert_eq!(split_string(input, seperated), output);
-    }
-    
-    #[test]
-    fn test2() {
-        // 区切るものがない場合
-        let input = "helloworld!!!";
-        let output =  (None, input);
-        let seperated = ' ';
-        assert_eq!(split_string(input, seperated), output);
+    fn t1() {
+        let input = "abc";
+        assert_eq!(char(input, 'a'), Some(((), "bc")));
     }
 
     #[test]
-    fn test3() {
-        // 連続して区切る場合
-        let data = "red,blue,gre en";
-        let pattern = ',';
-
-        let (output, data) = split_string(data, pattern);
-        assert_eq!(output, Some("red"));
-
-        let (output, data) = split_string(data, pattern);
-        assert_eq!(output, Some("blue"));
-
-        let (output, data) = split_string(data, pattern);
-        assert_eq!(output, None);
-        assert_eq!(data, "gre en");
+    fn t2() {
+        let input = "abc";
+        assert_eq!(char(input, 'b'), None);
     }
 
     #[test]
-    #[should_panic]
-    fn test4() {
-        // 入力が空白の場合
+    fn t3() {
         let input = "";
-        let seperated = ' ';
-        split_string(input, seperated);
+        assert_eq!(char(input, ' '), None);
     }
 }
