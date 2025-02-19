791430a4aa003f5150699963413d07fda3953e80
nikki9750 <76221172+tam1192@users.noreply.github.com>
2025-02-19 19:15:08 +0900

    エラーを実装

--- src/simple_perser/string_perser.rs
+++ src/simple_perser/string_perser.rs
@@ -1,2 +1,25 @@
-type Output<INPUT, OUTPUT> = Option<(INPUT, OUTPUT)>;
-trait Parser<INPUT, OUTPUT>: Fn(INPUT) -> Output<INPUT, OUTPUT> {}
+use std::{error, fmt};
+
+#[derive(Debug)]
+enum Error {
+    Test(String),
+}
+
+impl fmt::Display for Error {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Self::Test(s) => write!(f, "{}", s),
+        }
+    }
+}
+
+impl error::Error for Error {
+    fn source(&self) -> Option<&(dyn error::Error + 'static)> {
+        None 
+    }
+}
+
+enum Result<T> {
+    Ok(T),
+    Err(Error),
+}
