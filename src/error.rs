20dfd3b12c643b282ebebe9105c44fa39f095484
nikki9750 <76221172+tam1192@users.noreply.github.com>
2025-02-20 23:48:35 +0900

    足し算パーサー（超簡易）が完成 (#6)
    * 関数作成
    
    * テストを設計
    
    * spaceを除去できる関数が登場
    
    * 数値解析機能を追加し、整数と浮動小数点数を処理する関数を実装
    
    * 数値解析機能を改善し、整数と浮動小数点数の処理を強化
    
    * エラーの出力を最適化
    
    * コードをスマートにした
    
    * ParseErrorをノーコストで動作できるようにした
    
    * [cfg(test)]が抜けてたので追加
    
    * 計算記号（オペ）対応準備
    
    * コンパイルエラー回避
    
    * コンパイラ注意回避

--- src/error.rs
+++ src/error.rs
@@ -1,42 +1,51 @@
 use std::{error, fmt};
 
 #[derive(Debug, PartialEq)]
-pub enum Error {
+pub enum Error<T> 
+{
     ParseIntErrror(std::num::ParseIntError),
     ParseFloatError(std::num::ParseFloatError),
     ParseCharError,
+    Uninstalled,
+    ParseError(T),
 }
 
-impl fmt::Display for Error {
+impl<T> fmt::Display for Error<T> 
+where T: fmt::Debug + fmt::Display
+{
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
             Self::ParseFloatError(parse_int_error) => write!(f, "{}", parse_int_error),
             Self::ParseIntErrror(parse_int_error) => write!(f, "{}", parse_int_error),
             Self::ParseCharError => write!(f, "ParseCharError"),
+            Self::Uninstalled => write!(f, "未実装ですまない..."),
+            Self::ParseError(s) => write!(f, "ParseError: {}", s),
         }
     }
 }
 
-impl error::Error for Error {
+impl<T> error::Error for Error<T>
+where T: fmt::Debug + fmt::Display
+{
     fn source(&self) -> Option<&(dyn error::Error + 'static)> {
         match self {
             Self::ParseFloatError(e) => Some(e),
             Self::ParseIntErrror(e) => Some(e),
-            Self::ParseCharError => None,
+            _ => None
         }
     }
 }
 
-impl From<std::num::ParseFloatError> for Error {
+impl<T> From<std::num::ParseFloatError> for Error<T> {
     fn from(e: std::num::ParseFloatError) -> Self {
         Error::ParseFloatError(e)
     }
 }
 
-impl From<std::num::ParseIntError> for Error {
+impl<T> From<std::num::ParseIntError> for Error<T> {
     fn from(e: std::num::ParseIntError) -> Self {
         Error::ParseIntErrror(e)
     }
 }   
 
-pub type Result<T> = std::result::Result<T, Error>;
+pub type Result<T, E> = std::result::Result<T, Error<E>>;
---
+++ src/keisan.rs
@@ -0,0 +1,89 @@
+use crate::{error::*, parser::simple::*};
+
+#[derive(Debug)]
+pub enum OPs {
+    Add,
+    Neg,
+    Mul,
+    Div,
+}
+
+impl OPs {
+    fn _new<'a>(input: &'a str) -> Result<(&'a str, Self), &'a str> {
+        let input = input.trim_start();
+        match input.trim_ascii_start().chars().next() {
+        //     Some('+') => Ok((input, Self::Add)),
+        //     Some('-') => Ok((input, Self::Neg)),
+        //     Some('*') => Ok((input, Self::Mul)),
+        //     Some('/') => Ok((input, Self::Div)),
+            _ => Err(Error::ParseCharError),
+        }
+    }
+}
+
+
+
+pub fn parser<'a>(input: &'a str) -> Result<(&'a str, Number), &'a str> {
+    let num_parse = space_trimer(num_ex);
+    let sym_parse = space_trimer(char('+'));
+    
+    // first value
+    let (input, first) = num_parse(input)
+        .map_err(|_| Error::ParseError(input))?;
+    // synbol check
+    let (input, _) = sym_parse(input)
+        .map_err(|_| Error::ParseError(input))?;
+    // second value
+    let (input, second) = num_parse(input)
+        .map_err(|_| Error::ParseError(input))?;
+
+    if let Number::Int(first) = first {
+        if let Number::Int(second) = second {
+            return Ok((input, Number::Int(first + second)));
+        }
+    }
+    let first = match first {
+        Number::Int(x) => x as f64,
+        Number::Float(x) => x,
+    };
+    let second = match second {
+        Number::Int(x) => x as f64,
+        Number::Float(x) => x,
+    };
+    Ok((input, Number::Float(first + second)))
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    #[test]
+    fn test1() {
+        let base = "1+1";
+        assert_eq!(parser(base), Ok(("", Number::Int(2))));
+    }
+
+    #[test]
+    fn test2() {
+        let base = "2 + 4";
+        assert_eq!(parser(base), Ok(("", Number::Int(6))));
+    }
+
+    #[test]
+    fn test3() {
+        let base = "aa1+1ffad";
+        assert_eq!(parser(base), Err(Error::ParseError("aa1+1ffad")));
+    }
+
+    #[test]
+    fn test4() {
+        let base = "1 + a 1";
+        assert_eq!(parser(base), Err(Error::ParseError(" a 1")));
+    }
+
+    #[test]
+    fn test5() {
+        let base = "1 + 1aaaaaa";
+        assert_eq!(parser(base), Ok(("aaaaaa", Number::Int(2))));
+    }
+
+}
