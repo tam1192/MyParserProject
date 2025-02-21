e03c105e3faf8e483a1099d9c72b322a9fd32a20
nikki9750 <76221172+tam1192@users.noreply.github.com>
2025-02-21 21:04:25 +0900

    よりシンプルに修正

--- src/parser.rs
+++ src/parser.rs
@@ -10,6 +10,24 @@ pub enum Number {
     Float(f64),
 }
 
+impl From<Number> for f64 {
+    fn from(value: Number) -> Self {
+        match value {
+            Number::Int(x) => x as f64,
+            Number::Float(x) => x,
+        }
+    }
+}
+
+impl From<Number> for i64 {
+    fn from(value: Number) -> Self {
+        match value {
+            Number::Int(x) => x,
+            Number::Float(x) => x as i64,
+        }
+    }
+}
+
 impl From<f64> for Number {
     fn from(value: f64) -> Self {
         Self::Float(value)
@@ -26,22 +44,13 @@ impl Add for Number {
     type Output = Number;
 
     fn add(self, rhs: Self) -> Self::Output {
-        match self {
-            Number::Int(x) => {
-                let y = match rhs {
-                    Self::Int(y) => return Self::Int(x + y),
-                    Number::Float(y) => y,
-                };
-                let x = x as f64;
-                Self::Float(x + y)
-            },
-            Number::Float(x) => {
-                let y = match rhs {
-                    Number::Int(y) => y as f64,
-                    Number::Float(y) => y,
-                };
-                Self::Float(x + y)
-            },
+        match (self, rhs) {
+            (Number::Int(x), Number::Int(y)) => Number::Int(x+y),
+            _ => {
+                let x = f64::from(self);
+                let y = f64::from(self);
+                Number::Float(x+y)
+            }
         }
     }
 }
