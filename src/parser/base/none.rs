f64c959288032dc94e459d43f3f9a1e9f0bcee75
nikki9750 <76221172+tam1192@users.noreply.github.com>
2025-02-23 17:08:36 +0900

    noneのミスを修正

--- src/parser/base/none.rs
+++ src/parser/base/none.rs
@@ -1,14 +1,27 @@
-pub fn none<'a>(i: &'a str) -> Result<(&'a str, ()), &'a str> {
+use crate::error::Result;
+
+pub fn none<'a>(i: &'a str) -> Result<(&'a str, ())> {
     Ok((i, ()))
 }
 
 #[cfg(test)]
 mod char_test {
+    use crate::parser::{char, num, AndParse, OrParse};
+
     use super::*;
 
     #[test]
     fn test1() {
         let base = "/123";
         assert_eq!(none(base), Ok(("/123", ())));
+    }
+
+    #[test]
+    fn test2() {
+        let base1 = "+123";
+        let base2 = "123";
+        let parser = char('+').or(none).and_b(num);
+        assert_eq!(parser(base1), Ok(("", 123)));
+        assert_eq!(parser(base2), Ok(("", 123)));
     }
 }
