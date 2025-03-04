cf8c47f7670e9a62eb5f776c50889c43b0c0a352
nikki9750 <76221172+tam1192@users.noreply.github.com>
2025-03-04 23:16:44 +0900

    parser, base, combinatorの説明追加

---
+++ src/parser.rs
@@ -0,0 +1,10 @@
+/// 単体でパーサーとして動作する、もっとも基本的なパーサー群。
+pub mod base;
+
+/// 複数のパーサーを組み合わせて動作するパーサー
+pub mod combinator;
+
+/// パーサー関数をトレイトオブジェクト化
+trait Parser<I, T, E>: Fn(I) -> (I, Result<T, E>) + Clone {}
+// Fn(I) -> (I, Result<T,E>) を Parser<I, T, E> として使えるようにする
+impl<I, T, E, F> Parser<I, T, E> for F where F: Fn(I) -> (I, Result<T, E>) + Clone {}
