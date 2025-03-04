9b6184b4edcd1df1d62f775bfb3ab6e429182949
nikki9750 <76221172+tam1192@users.noreply.github.com>
2025-03-05 00:05:38 +0900

    numの仕様を定義

--- src/parser/base/num.rs
+++ src/parser/base/num.rs
@@ -15,3 +15,25 @@
 pub fn num<'a>(i: &'a str) -> (&'a str, Result<i64, crate::parser::Error>) {
     todo!()
 }
+
+#[cfg(test)]
+mod tests{
+    use super::*;
+
+    // 正常系: 数字がパースできる場合
+    fn success_test() {
+        let input = "123abc";
+        let (rest, result) = num(input);
+        assert_eq!(result, Ok(123));
+        assert_eq!(rest, "abc");
+    }
+
+    // 異常系: 数字がパースできない場合
+    fn dissociation_test() {
+        let input = "abc123";
+        let (rest, result) = num(input);
+        assert!(matches!(result, Err(_)));
+        assert_eq!(rest, "abc123");
+    }
+
+}
--- src/parser/error/errorkind.rs
+++ src/parser/error/errorkind.rs
@@ -1,4 +1,5 @@
 /// [super::Error] で使用する、ジャンル分けされたエラー原因を格納するenum
+#[derive(PartialEq, Debug)]
 pub enum ErrorKind{
     /// パースできない時
     ParseNotNum,
