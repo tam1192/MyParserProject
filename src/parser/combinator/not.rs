9b188af499ca4d1a9f9b6b16c9b48f2376d6dada
nikki9750 <76221172+tam1192@users.noreply.github.com>
2025-04-27 12:22:48 +0900

    testを埋めた

--- src/parser/combinator/not.rs
+++ src/parser/combinator/not.rs
@@ -19,3 +19,38 @@ use super::*;
 pub fn not<I, T, E, P: Parser<I, Result<T, E>>>(p: P) -> impl Parser<I, I> {
     move |i| todo!()
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::parser::str_parser::*;
+
+    #[test]
+    fn simple() {
+        let input = "abc123";
+        let parser = not(num);
+        let (remain, result) = parser(input);
+        assert_eq!(result, "abc");
+        assert_eq!(remain, "123");
+    }
+
+    // 入力パーサーがすぐに成功しちゃう例
+    #[test]
+    fn zero_length_remain() {
+        let input = "123";
+        let parser = not(num);
+        let (remain, result) = parser(input);
+        assert_eq!(result, "123");
+        assert_eq!(remain, "");
+    }
+
+    // 入力パーサーがすべて失敗する例
+    #[test]
+    fn input_parser_always_fail() {
+        let input = "abc";
+        let parser = not(num);
+        let (remain, result) = parser(input);
+        assert_eq!(result, "");
+        assert_eq!(remain, "abc");
+    }
+}
