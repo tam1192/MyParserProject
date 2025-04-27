338dc2164d9df6e41b40ac0e39a9401c2feb9302
nikki9750 <76221172+tam1192@users.noreply.github.com>
2025-04-27 11:50:33 +0900

    ドキュメント完成

--- src/parser/combinator/not.rs
+++ src/parser/combinator/not.rs
@@ -1,5 +1,21 @@
 use super::*;
 
-pub fn not<I, R, P: Parser<I, R>>(p: P) -> impl Parser<I, Result<I, Error>> {
+/// [Parser]を成功させるまでに使った入力を取る
+///
+/// 戻り値が[Result]の[Parser]を、成功するまで入力をずらしながら試し、ずらした分の入力を結果として返します。
+///
+/// # Example
+/// ```
+/// use my_parser_project::parser::{combinator::*, str_parser::*};
+///  
+/// let base = "abc123"
+/// let parser = not(num);
+/// let (remain, result) = parser(base);
+///
+/// assert_eq!(result, "abc");
+/// assert_eq!(remain, "123");
+/// ```
+///
+pub fn not<I, T, E, P: Parser<I, Result<T, E>>>(p: P) -> impl Parser<I, I> {
     move |i| todo!()
 }
